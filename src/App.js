9a2b7e57a984c842ae0022c9ee08c1388d0ce03f
vinoffandrey <Vinogradow.Andrej@yandex.ru>
2022-04-09 20:56:51 +0300

    Second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { Component, StrictMode } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = () => {
+	return <h2>Hello world!</h2>
 }
 
+// const Field = () => {
+// 	const holder = "Enter here";
+// 	const styledField = {
+// 		width: '300px'
+// 	};
+// 	return <input
+// 		placeholder={holder}
+// 		type="text"
+// 		style={styledField} />
+// }
+
+class Field extends Component {
+	render() {
+		const holder = "Enter here";
+		const styledField = {
+			width: '300px'
+		};
+
+		return <input
+			placeholder={holder}
+			type="text"
+			style={styledField} />
+	}
+}
+
+function Btn() {
+	const text = 'Log in';
+	const logged = true;
+
+	// const res = () => {
+	// 	return 'Log in';
+	// }
+	// const p = <p>Log in</p>;
+	return <button>{logged ? 'Enter' : text}</button>
+}
+
+function App() {
+	return (
+		<div className="App">
+			<StrictMode>
+				<Header />
+			</StrictMode>
+			<Field />
+			<Btn />
+		</div>
+	);
+}
+
+export { Header };
 export default App;
